d2b705afaf89320af2a6a505cb7589f30637f5e7
afrizal423 <afrizal2499@gmail.com>
2020-08-17 00:33:16 +0700

    docker fix

--- crudtutorial/settings.py
+++ crudtutorial/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j$t3i*)z#npdycc6e!+qd&)_u@6d-6lpn!qw*hq(9u)!f=ocpg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'crudtutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#         'ENGINE': 'django.db.backends.mysql', 
-#         'NAME': 'django_tutorialcrud',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': '127.0.0.1',   
-#         'PORT': '3306',
-#     }   
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'django_tutorialcrud',
+        'USER': 'user',
+        'PASSWORD': 'user',
+        'HOST': 'mysql',   
+        'PORT': '3306',
+    }   
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
